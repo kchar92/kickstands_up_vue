886006317b243ccb661448e39232e9ce3dbe5bc7
Keelana Char <keelanachar@gmail.com>
2019-12-26 16:19:06 -0600

    add buttons to show all rides, and only the rides the current_user is attending

--- src/views/RidesIndex.vue
+++ src/views/RidesIndex.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
+    <button v-on:click="myRides()">My rides</button>
+    <button v-on:click="otherRides()">See other rides</button>
     <h1>{{ message }}</h1>
     <div v-for="ride in rides">
       <p>Name: {{ ride.ride }}</p>
       <p>Date & Time: {{ ride.date_time }}</p>
       <p>Starting: {{ ride.starting_point }}</p>
       <p>Ending: {{ ride.ending_point }}</p>
-      <p>Distance: {{ ride.distance }} miles</p>
+      <p>Distance: {{ ride.distance }}</p>
       <p>Bike Type: {{ ride.bike_type }}</p>
       <hr>
     </div>
@@ -21,16 +23,27 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Rides index",
+      message: "Rides",
       rides: []
     };
   },
   created: function() {
     axios.get("/api/rides").then(response => {
-      console.log(response.data);
       this.rides = response.data;
     });
   },
-  methods: {}
+  methods: {
+    myRides: function() {
+      axios.get("/api/rides?attending=true").then(response => {
+        this.rides = response.data;
+      });
+      
+    },
+    otherRides: function() {
+      axios.get("/api/rides").then(response => {
+        this.rides = response.data;
+      });
+    }
+  }
 };
 </script>
